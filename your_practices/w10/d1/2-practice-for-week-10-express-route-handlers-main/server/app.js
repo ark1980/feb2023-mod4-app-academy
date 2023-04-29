368f293dea2f14e0c67bbf8a13b74a6565b274a6
Ali <ar.k@outlook.com>
2023-04-29 11:41:35 -0700

    d1 long practice complete

--- your_practices/w10/d1/2-practice-for-week-10-express-route-handlers-main/server/app.js
+++ your_practices/w10/d1/2-practice-for-week-10-express-route-handlers-main/server/app.js
@@ -49,6 +49,88 @@ app.get("/artists/latest/albums", (req, res) => {
   res.json(getAlbumsForLatestArtist());
 });
 
+app.get("/artists/:artistId", (req, res) => {
+  res.json(getArtistByArtistId(req.params.artistId));
+})
+
+app.put("/artists/:artistId", (req, res) => {
+  res.json(editArtistByArtistId(req.params.artistId, req.body));
+});
+
+app.patch("/artists/:artistId", (req, res) => {
+  res.json(editArtistByArtistId(req.params.artistId, req.body));
+});
+
+app.delete("/artists/:artistId", (req, res) => {
+  deleteArtistByArtistId(req.params.artistId);
+  res.json({ message: 'Successfully deleted' });
+});
+
+app.get("/artists/:artistId/albums", (req, res) => {
+  res.json(getAlbumsByArtistId(req.params.artistId));
+})
+
+app.get("/albums/:albumId", (req, res) => {
+  res.json(getAlbumByAlbumId(req.params.albumId));
+})
+
+app.post("/artists/:artistId/albums", (req, res) => {
+  res.status(201).json(addAlbumByArtistId(req.params.artistId, req.body));
+});
+
+app.put("/albums/:albumId", (req, res) => {
+  res.json(editAlbumByAlbumId(req.params.albumId, req.body));
+});
+
+app.patch("/albums/:albumId", (req, res) => {
+  res.json(editAlbumByAlbumId(req.params.albumId, req.body));
+});
+
+app.delete("/albums/:albumId", (req, res) => {
+  deleteAlbumByAlbumId(req.params.albumId);
+  res.json({ message: "Successfully deleted" });
+});
+
+app.get("/albums", (req,res) => {
+  res.json(getFilteredAlbums(req.query.startsWith));
+});
+
+app.get("/songs/:songId", (req,res) => {
+  res.json(getSongBySongId(req.params.songId));
+})
+
+app.post("/albums/:albumId/songs", (req,res) => {
+  res.status(201).json(addSongByAlbumId(req.params.albumId, req.body));
+});
+
+app.get("/artists/:artistId/songs", (req, res) => {
+  res.json(getSongsByArtistId(req.params.artistId));
+})
+
+app.get("/albums/:albumId/songs", (req, res) => {
+  res.json(getSongsByAlbumId(req.params.albumId));
+});
+
+app.put("/songs/:songId", (req, res) => {
+  const songId = req.params.songId;
+  const data = req.body;
+  res.json(editSongBySongId(songId, data));
+});
+
+app.patch("/songs/:songId", (req, res) => {
+  const songId = req.params.songId;
+  const data = req.body;
+  res.json(editSongBySongId(songId, data));
+});
+
+app.delete("/songs/:songId", (req, res) => {
+  deleteSongBySongId(req.params.songId);
+  res.json({ message: "Successfully deleted" });
+})
+
+
+
+
 // DO NOT MODIFY
 if (require.main === module) {
   const port = 8000;
